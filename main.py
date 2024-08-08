73f47c301eca9c0f4919d6aa2414491fd85d7ac2
Chris Tullier <christopher.tullier98@gmail.com>
2024-08-08 10:28:41 -0400

    adjustments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import rawpy
 
+from movie import make_video
+
 
 def adjust_tone_curve(image, curve_points_red, curve_points_green, curve_points_blue):
     # Create lookup tables for each color channel
@@ -9,11 +11,13 @@ def adjust_tone_curve(image, curve_points_red, curve_points_green, curve_points_
     lut_red = np.interp(
         full_range, [p[0] for p in curve_points_red], [p[1] for p in curve_points_red]
     ).astype("uint8")
+
     lut_green = np.interp(
         full_range,
         [p[0] for p in curve_points_green],
         [p[1] for p in curve_points_green],
     ).astype("uint8")
+
     lut_blue = np.interp(
         full_range, [p[0] for p in curve_points_blue], [p[1] for p in curve_points_blue]
     ).astype("uint8")
@@ -33,50 +37,59 @@ def adjust_tone_curve(image, curve_points_red, curve_points_green, curve_points_
 
 
 def adjust_exposure(image, exposure_factor):
-    """
-    Adjust the exposure of an image.
-
-    Parameters:
-        image (numpy.ndarray): Input image.
-        exposure_factor (float): Factor by which to adjust the exposure.
-                                 Values > 1 will increase exposure, values < 1 will decrease exposure.
-
-    Returns:
-        numpy.ndarray: Image with adjusted exposure.
-    """
     return np.clip(image * exposure_factor, 0, 255).astype("uint8")
 
 
-# Load an image
-# image = cv2.imread("tree.jpg", cv2.IMREAD_COLOR)
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
-raw = rawpy.imread("tree.NEF")
-image = raw.postprocess()
+def adjust_images(
+    red: list[tuple[int, int]],
+    green: list[tuple[int, int]],
+    blue: list[tuple[int, int]],
+    frames: int,
+    dir: str,
+):
+    raw = rawpy.imread("tree.NEF")
+    image = raw.postprocess()
 
-TEST = False
+    final_exposure_adjustment = 4
+    for i in range(frames):
+        # Slightly increase the exposure
+        exposure_factor = round(final_exposure_adjustment * (i / frames), 4)
 
-final_adjustment = 4
-if TEST:
-    frames = 10
-    dir = "test"
-else:
-    frames = 120 * 2
-    dir = "output"
-for i in range(frames):
-    # Slightly increase the exposure
-    exposure_factor = round(final_adjustment * (i / frames), 4)
+        # exposure **must** be adjusted first
+        adjusted_image = adjust_exposure(image, exposure_factor)
 
-    # exposure **must** be adjusted first
-    adjusted_image = adjust_exposure(image, exposure_factor)
+        # Define tone curve points for each channel (example)
+        # flat: [(0, 0), (64, 80), (128, 128), (192, 200), (255, 255)]
+        curve_points_red = red
+        curve_points_green = green
+        curve_points_blue = blue
 
-    # Define tone curve points for each channel (example)
-    # base: [(0, 0), (64, 80), (128, 128), (192, 200), (255, 255)]
-    # Adjust the tone curves of the image
-    curve_points_red = [(30, 0), (130 + i, 255), (200, 0)]
-    curve_points_green = [(0, 0), (89 + i, 255), (255, 0)]
-    curve_points_blue = [(80, 0), (100 + i, 255), (255, 0)]
+        # Adjust the tone curves of the image
+        adjusted_image = adjust_tone_curve(
+            adjusted_image, curve_points_red, curve_points_green, curve_points_blue
+        )
+        cv2.imwrite(f"{dir}/{i+1}.png", adjusted_image)
 
-    adjusted_image = adjust_tone_curve(
-        adjusted_image, curve_points_red, curve_points_green, curve_points_blue
-    )
-    cv2.imwrite(f"{dir}/{i+1}.png", adjusted_image)
+
+if __name__ == "__main__":
+    vid_name = "8"
+
+    TEST = True
+    curve_adjustments = {
+        "red": [(30, 0), (130, 255), (200, 0)],
+        "green": [(0, 0), (89, 255), (255, 0)],
+        "blue": [(80, 0), (100, 255), (150, 0)],
+    }
+    if TEST:
+        frames = 24
+        dir = "test"
+    else:
+        frames = 24 * 8  # 8 seconds of video
+        dir = "output"
+
+    if TEST:
+        adjust_images(**curve_adjustments, frames=frames, dir=dir)
+        make_video(vid_name, "test", 24)
+    else:
+        adjust_images(**curve_adjustments, frames=frames, dir=dir)
+        make_video(vid_name, "output", 24)
---
+++ movie.py
@@ -0,0 +1,62 @@
+import os
+import re
+
+import cv2
+
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+
+def alphanum_key(s):
+    """Turn a string into a list of string and number chunks.
+    "z23a" -> ["z", 23, "a"]
+    """
+    return [tryint(c) for c in re.split("([0-9]+)", s)]
+
+
+def sort_nicely(l):
+    """Sort the given list in the way that humans expect."""
+    l.sort(key=alphanum_key)
+
+
+def images_to_video(image_folder, output_video, frame_rate):
+    images = [
+        img
+        for img in os.listdir(image_folder)
+        if img.endswith(".png") or img.endswith(".jpg")
+    ]
+    # images.sort()  # Ensure the images are sorted in the correct order
+    sort_nicely(images)
+
+    # Read the first image to get the dimensions
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec for .mp4 files
+    video = cv2.VideoWriter(output_video, fourcc, frame_rate, (width, height))
+
+    for image in images:
+        img_path = os.path.join(image_folder, image)
+        frame = cv2.imread(img_path)
+        video.write(frame)
+        print(f"wrote: {image}")
+
+    video.release()
+    print(f"Video saved as {output_video}")
+
+
+def make_video(video_file_name: str, image_folder: str, frame_rate: int):
+
+    video_file_name = "vids/" + video_file_name + ".mp4"
+
+    images_to_video(image_folder, video_file_name, frame_rate)
+    os.system("say completo")
+
+
+if __name__ == "__main__":
+    make_video()
